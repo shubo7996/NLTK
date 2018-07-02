9da8cb0d6f5f750eab23280c921e1b637e9c99b1
shubo7996 <whatsupmofo420@gmail.com>
2018-07-03 03:45:09 +0530

    Stop Words

--- nltk2.py
+++ nltk2.py
@@ -3,5 +3,17 @@ from nltk.tokenize import word_tokenize
 
 example_sentence = " hey maa, i'm coming home"
 stop_words = set(stopwords.words("english"))
+words = word_tokenize(example_sentence)
 
-print(stop_words)
+#print(stop_words)
+
+filtered_sentence = []
+
+for w in words:
+    if w not in stop_words:
+        filtered_sentence.append(w)
+
+print (filtered_sentence)
+
+
+
