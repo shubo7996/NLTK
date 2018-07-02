2bcc2838d5293e644f38dcb17a682ed2a0824693
shubo7996 <whatsupmofo420@gmail.com>
2018-07-03 04:38:59 +0530

    speech Tagging

--- nltk4.py
+++ nltk4.py
@@ -42,7 +42,21 @@ from nltk.tokenize import PunktSentenceTokenizer
 
 
 
+
 train text = state_union.raw("2005-GWBush.txt")
 sample_text = state_union.raw("2006-GWBush.txt")
-custom_sent_tokenizer = PunktSentenceTokenizer(sample_text)
+custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
 
+tokenized = custom_sent_tokenizer(sample_text)
+
+def process_content():
+    try: 
+        for i in tokenized:
+            words = nltk.word_tokenize(i)
+            tagged = nltk.pos_tag(words)
+            print (tagged)
+
+    except Exception as e:
+        print (str(e))
+
+process_content()
