7aa3662c4bc53e156418788fbb8453c71330a292
shubo7996 <whatsupmofo420@gmail.com>
2018-07-03 03:21:19 +0530

    Intro

---
+++ nltk1.py
@@ -0,0 +1,16 @@
+from nltk import 
+
+nltk.download()
+
+#tokenizers
+
+#word tokenizer - seperating by word
+#sentence tokenizer - seperated by sentences
+
+#lexicon and corporas
+#corpora - body of text ex: presenditial speech, a paragraph from your book.
+#lexicon - word and their mean (dictionary)
+#words with dual meaning depending on the sentence
+
+
+
