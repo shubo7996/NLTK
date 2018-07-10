91238c7d22b96a4dc404b035c40d3669b9510ecf
shubo7996 <whatsupmofo420@gmail.com>
2018-07-10 15:41:07 +0530

    Entity Recognization

--- nltk7.py
+++ nltk7.py
@@ -9,3 +9,17 @@ custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
 
 tokenized = custom_sent_tokenizer.tokenize(sample_text)
 
+def process_content():
+    try:
+        for i in tokenized[5:]:
+            words = nltk.word_tokenize(i)
+            tagged = nltk.pos_tag(words)
+            namedEnt = nltk.ne_chunk(tagged, binary=True)
+            namedEnt.draw()
+
+    except Exception as e:
+        print(str(e))
+
+process_content()
+
+    
