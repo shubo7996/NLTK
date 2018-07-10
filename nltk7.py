8b8b67f4a2e591659861f19e050b591a801ecfb9
shubo7996 <whatsupmofo420@gmail.com>
2018-07-10 15:34:44 +0530

    Entity Recognization

---
+++ nltk7.py
@@ -0,0 +1,11 @@
+import  nltk
+from nltk.corpus import state_union
+from nltk.tokenize import PunktSentenceTokenizer
+
+train_text = state_union.raw("2005-GWBush.txt")
+sample_text = state_union.raw("2006-GWBush.txt")
+
+custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
+
+tokenized = custom_sent_tokenizer.tokenize(sample_text)
+
