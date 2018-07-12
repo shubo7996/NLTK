d833eafeedede8749f028a73c47c8ced679a0fb4
shubo7996 <whatsupmofo420@gmail.com>
2018-07-12 23:04:52 +0530

    Lemmatize

---
+++ nltk8.py
@@ -0,0 +1,9 @@
+from nltk import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+print (lemmatizer.lemmatize("cats"))
+print (lemmatizer.lemmatize("gerald"))
+print (lemmatizer.lemmatize("hounds"))
+print (lemmatizer.lemmatize("timely"))
+print (lemmatizer.lemmatize("python"))
